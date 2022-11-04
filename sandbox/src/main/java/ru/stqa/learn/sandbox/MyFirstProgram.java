2ad064eaf968537b7b3c7499396d96ea22e3d182
Pearl1306 <linatali1306@gmail.com>
2022-11-03 20:17:12 -0400

    several function added

--- sandbox/src/main/java/ru/stqa/learn/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/learn/sandbox/MyFirstProgram.java
@@ -2,10 +2,26 @@ package ru.stqa.learn.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        hello("world");
+        hello("Danya");
+        double len = 5;
+        System.out.println("Square area with side " + len + " equals " + area (len));
+        double a = 4;
+        double b = 5;
+        System.out.println("Square rectangle with sides "+ a+ " and "+b +" equals "+ area(a,b));
 
-         System.out.println("Hello World!");
+    }
 
-     }
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+    
+    public static double area(double l){
+        return l*l;
+    }
+    public static double area(double a, double b){
+        return a*b;
+    }
 
-  }
+}
