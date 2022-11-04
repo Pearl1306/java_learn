81f67a5c85bc41780cb04a335661c97615b51c6e
Pearl1306 <linatali1306@gmail.com>
2022-11-04 06:44:35 -0400

    Homework 2
    вычисление расстояния между двумя точками) при помощи метода в классе Point

--- sandbox/src/main/java/ru/stqa/learn/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/learn/sandbox/MyFirstProgram.java
@@ -10,22 +10,15 @@ public class MyFirstProgram {
         Rectangle r = new Rectangle(4,5);
 
         System.out.println("Square rectangle with sides "+ r.a+ " and "+r.b +" equals "+ r.area());
-        Point p1 = new Point();
-        Point p2 = new Point();
-        p1.x1= 5;
-        p1.y1=3;
-        p2.x2=10;
-        p2.y2=7;
+        Point p1 = new Point(5,10,3,7);
 
-        System.out.println("Distance between two points = " + distance(p1,p2));
+        System.out.println("Distance between two points = " + p1.distance());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello " + somebody + "!");
     }
-    public static double distance(Point p1, Point p2){
-return Math.sqrt((p2.x2- p1.x1)*(p2.x2-p1.x1)+(p2.y2-p1.y1)*(p2.y2-p1.y1));
-    }
+
 
 
 
