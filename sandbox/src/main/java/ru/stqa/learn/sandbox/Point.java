2b1763d2204a93684784cbc2ad8d59c2a6560aa7
Pearl1306 <linatali1306@gmail.com>
2022-11-06 19:16:47 -0500

    update homework 2

--- sandbox/src/main/java/ru/stqa/learn/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/learn/sandbox/Point.java
@@ -1,19 +1,18 @@
 package ru.stqa.learn.sandbox;
 
 public class Point {
-    public double x2;
-    public double x1;
-    public double y1;
-    public double y2;
-    public Point(double x1, double y1){
-        this.x1=x1;
 
-        this.y1=y1;
+    public double x;
+    public double y;
+    public Point(double x1, double y1){
+        this.x=x;
+
+        this.y =y;
 
     }
 
    public double distance(Point p2){
-       return Math.sqrt((p2.x1- this.x1)*(p2.x1-this.x1)+(p2.y1-this.y1)*(p2.y1-this.y1));
+       return Math.sqrt((p2.x- this.x)*(p2.x-this.x)+(p2.y -this.y)*(p2.y -this.y));
    }
 
 
