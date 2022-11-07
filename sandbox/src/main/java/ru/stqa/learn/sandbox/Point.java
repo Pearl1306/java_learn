e408fb0fe568589c2a849c53c2bbb57a3a1f9b40
Pearl1306 <linatali1306@gmail.com>
2022-11-07 03:16:38 -0500

    snova izmeneny parametry metoda Homework 2

--- sandbox/src/main/java/ru/stqa/learn/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/learn/sandbox/Point.java
@@ -4,7 +4,7 @@ public class Point {
 
     public double x;
     public double y;
-    public Point(double x1, double y1){
+    public Point(double x, double y){
         this.x=x;
 
         this.y =y;
