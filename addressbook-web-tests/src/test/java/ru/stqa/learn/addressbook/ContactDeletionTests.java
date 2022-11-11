f3b02a19570fd0d44b8968895efa7fcbf7298ec6
Pearl1306 <linatali1306@gmail.com>
2022-11-10 20:37:46 -0500

    Pull Contact methods to TestBase
    General methods from ContactCreation tests and Contact deletion tests were added to general class TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/ContactDeletionTests.java
@@ -1,60 +1,19 @@
- package ru.stqa.learn.addressbook;
+package ru.stqa.learn.addressbook;
 
-import java.util.concurrent.TimeUnit;
-
-import io.netty.util.AsciiString;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-public class ContactDeletionTests {
-  private WebDriver wd;
+public class ContactDeletionTests extends TestBase {
 
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-  }
+    @Test
+    public void testContactDeletion() throws Exception {
 
-  @Test
-  public void testContactDeletion() throws Exception {
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.id("LoginForm")).click();
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-    wd.findElement(By.xpath("//td/input")).click();
-    wd.findElement(By.xpath("//input[@value='Delete']")).click();
-    wd.switchTo().alert().accept();
-    wd.findElement(By.linkText("Logout")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-  }
+        selectContact();
+        deleteContact();
+        acceptDelitionContact();
 
-
-
-
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
     }
-  }
 
 
 }
