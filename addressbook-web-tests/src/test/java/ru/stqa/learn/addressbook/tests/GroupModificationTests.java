5d6f19d993e8e0f40c8d0c0f6544575f36f8b9ea
Pearl1306 <linatali1306@gmail.com>
2022-11-12 01:32:33 -0500

    new test Groupmodification was created

---
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.learn.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.learn.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+    @Test
+    public void testModification(){
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test1"));
+        app.getGroupHelper().submitGroupmodification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
