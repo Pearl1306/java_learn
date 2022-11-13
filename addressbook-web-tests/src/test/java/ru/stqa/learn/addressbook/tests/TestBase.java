9fdec267981c4a5e9b9ea5f85088f39b4fd06e5b
Pearl1306 <linatali1306@gmail.com>
2022-11-13 07:18:38 -0500

    Starting tests in different browsers
    homework # 7

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/TestBase.java
@@ -1,13 +1,14 @@
 package ru.stqa.learn.addressbook.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.learn.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
 
 
