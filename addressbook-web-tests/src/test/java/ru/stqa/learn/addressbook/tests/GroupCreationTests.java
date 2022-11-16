f642dbd4ca4fab6842c9c98e20b90c87d0f5db98
Pearl1306 <linatali1306@gmail.com>
2022-11-16 06:18:57 -0500

    Quantity elements in collection
    Verification quantity Tests for group were created

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.learn.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.learn.addressbook.model.GroupData;
 
@@ -10,8 +11,12 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().goToGroupPage();
+        int before =app.getGroupHelper().getGroupCount();
         app.getGroupHelper().creatGroup(new GroupData("test2", null, null));
+        int after =app.getGroupHelper().getGroupCount();
         app.logout();
+        Assert.assertEquals(after,before + 1);
+
 
     }
 
--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.learn.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.learn.addressbook.model.GroupData;
 
@@ -7,6 +8,7 @@ public class GroupModificationTests extends TestBase{
     @Test
     public void testModification(){
         app.getNavigationHelper().goToGroupPage();
+        int before =app.getGroupHelper().getGroupCount();
         if(!app.getGroupHelper().isThereAgroup()){
             app.getGroupHelper().creatGroup(new GroupData("test2", null, null));
         }
@@ -15,5 +17,8 @@ public class GroupModificationTests extends TestBase{
         app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test1"));
         app.getGroupHelper().submitGroupmodification();
         app.getGroupHelper().returnToGroupPage();
+        int after =app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(before,after);
+
     }
 }
