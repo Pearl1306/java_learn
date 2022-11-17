043bbb29ee2308cf64917777790177cdec5c0b39
Pearl1306 <linatali1306@gmail.com>
2022-11-17 02:26:52 -0500

    Collection comparison for group deletion test

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/model/GroupData.java
@@ -1,4 +1,7 @@
 package ru.stqa.learn.addressbook.model;
+
+import java.util.Objects;
+
 //object
 public class GroupData {
     private final String header;
@@ -22,5 +25,27 @@ public class GroupData {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupDeletionTests.java
@@ -21,6 +21,9 @@ public class GroupDeletionTests extends TestBase {
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(before.size()-1,after.size());
+
+    before.remove(before.size()-1);
+    Assert.assertEquals(before,after);
   }
 
 
