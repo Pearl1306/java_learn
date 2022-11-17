241f96777f37ff8d13b55d34dabb3ea3956b317b
Pearl1306 <linatali1306@gmail.com>
2022-11-17 03:46:28 -0500

    Sets: unordered collections
    Group modification tests

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/model/GroupData.java
@@ -4,15 +4,27 @@ import java.util.Objects;
 
 //object
 public class GroupData {
+    private final String id;
     private final String header;
     private final String footer;
     private final String name;
 
     //constructor
-    public GroupData(String header, String footer, String name) {
+    public GroupData(String id,String header, String footer, String name) {
+        this.id = id;
         this.header = header;
         this.footer = footer;
         this.name = name;
+    }
+    public GroupData(String header, String footer, String name) {
+        this.id = null;
+        this.header = header;
+        this.footer = footer;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getHeader() {
@@ -34,18 +46,22 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
+        if (!Objects.equals(id, groupData.id)) return false;
         return Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
-        return name != null ? name.hashCode() : 0;
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 
     @Override
     public String toString() {
         return "GroupData{" +
-                "name='" + name + '\'' +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
                 '}';
     }
 }
