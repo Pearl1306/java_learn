d540c6f9c1f38f215a0bc14335984a88adcf7cff
Pearl1306 <linatali1306@gmail.com>
2022-11-21 06:53:55 -0500

    Caching the results of lengthy operations
    Кеширование результатов длительных операций для групп

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/appmanager/GroupHelper.java
@@ -51,6 +51,7 @@ public class GroupHelper extends HelperBase {
         initGroupCreation();
         fillGroupForm(group);
         submitGroupCreation();
+        groupCache=null;
         returnToGroupPage();
     }
     public void modify(GroupData group) {
@@ -58,11 +59,13 @@ public class GroupHelper extends HelperBase {
         initGroupModification();
         fillGroupForm(group);
         submitGroupmodification();
+        groupCache=null;
         returnToGroupPage();
     }
     public void delete(GroupData group) {
         selectGroupById(group.getId());
         deleteSelectedGroup();
+        groupCache=null;
         returnToGroupPage();
     }
 
@@ -84,15 +87,20 @@ public class GroupHelper extends HelperBase {
         }
         return groups;
     }
+    private Groups groupCache = null;
     public Groups all() {
-        Groups groups = new Groups();
+        if(groupCache!=null){
+            return new Groups(groupCache);
+        }
+
+        groupCache = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for(WebElement element : elements){
             String name = element.getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-            groups.add(new GroupData().withId(id).withName(name));
+            groupCache.add(new GroupData().withId(id).withName(name));
         }
-        return groups;
+        return new Groups(groupCache);
     }
 
 }
