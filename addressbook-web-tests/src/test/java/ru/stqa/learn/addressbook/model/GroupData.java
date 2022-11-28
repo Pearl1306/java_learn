cf816faf8c33e45615e43b04b3800257361a0138
Pearl1306 <linatali1306@gmail.com>
2022-11-28 03:12:52 -0500

    Format Gson for group

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/model/GroupData.java
@@ -1,5 +1,6 @@
 package ru.stqa.learn.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -8,9 +9,12 @@ import java.util.Objects;
 @XStreamAlias("group")
 public class GroupData {
     @XStreamOmitField
-    private  int id = Integer.MAX_VALUE;;
+    private  int id = Integer.MAX_VALUE;
+    @Expose
     private  String header;
+    @Expose
     private  String footer;
+    @Expose
     private  String name;
 
     public int getId() {
