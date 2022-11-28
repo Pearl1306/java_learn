4512f6b08b45e9af2c8000b6bc074571268abfd1
Pearl1306 <linatali1306@gmail.com>
2022-11-28 06:48:09 -0500

    Loading test data from file for contact

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/model/ContactData.java
@@ -4,152 +4,166 @@ import java.io.File;
 import java.util.Objects;
 
 public class ContactData {
-    private int id = Integer.MAX_VALUE;
-    private String firstname;
-    private String lastname;
-    private String address;
-    private String homephone;
-    private String mobilephone;
-    private String workphone;
-    private String allPhones;
-    private String email;
-    private String email2;
-    private String email3;
-    private String allEmails;
-    private File photo;
+  private int id = Integer.MAX_VALUE;
+  private String firstname;
+  private String lastname;
+  private String address;
+  private String homephone;
+  private String mobilephone;
+  private String workphone;
+  private String allPhones;
+  private String email;
+  private String email2;
+  private String email3;
+  private String allEmails;
+  private File photo;
 
-    public ContactData withPhoto(File photo) {
-        this.photo = photo;
-        return this;
-    }
+  public ContactData withPhoto(File photo) {
+    this.photo = photo;
+    return this;
+  }
 
-    public ContactData withId(int id) {
-        this.id = id;
-        return this;
-    }
-    public ContactData withFirstname(String firstname) {
-        this.firstname = firstname;
-        return this;
-    }
+  public ContactData withId(int id) {
+    this.id = id;
+    return this;
+  }
 
-    public ContactData withLastname(String lastname) {
-        this.lastname = lastname;
-        return this;
-    }
+  public ContactData withFirstname(String firstname) {
+    this.firstname = firstname;
+    return this;
+  }
 
-    public ContactData withAddress(String address) {
-        this.address = address;
-        return this;
-    }
-    public ContactData withMobilePhone(String mobilephone) {
-        this.mobilephone = mobilephone;
-        return this;
-    }
+  public ContactData withLastname(String lastname) {
+    this.lastname = lastname;
+    return this;
+  }
 
-    public ContactData withWorkphone(String workphone) {
-        this.workphone = workphone;
-        return this;
-    }
+  public ContactData withAddress(String address) {
+    this.address = address;
+    return this;
+  }
 
-    public ContactData withHomephone(String homephone) {
-        this.homephone = homephone;
-        return this;
-    }
-    public ContactData withAllPhones(String allPhones) {
-        this.allPhones = allPhones;
-        return this;
-    }
+  public ContactData withMobilePhone(String mobilephone) {
+    this.mobilephone = mobilephone;
+    return this;
+  }
 
-    public ContactData withEmail(String email) {
-        this.email = email;
-        return this;
-    }
-    public ContactData withEmail2(String email2) {
-        this.email2 = email2;
-        return this;
-    }
+  public ContactData withWorkphone(String workphone) {
+    this.workphone = workphone;
+    return this;
+  }
 
-    public ContactData withEmail3(String email3) {
-        this.email3 = email3;
-        return this;
-    }
-    public ContactData withAllEmails(String allEmails) {
-        this.allEmails = allEmails;
-        return this;
-    }
-    public int getId() {
-        return id;
-    }
+  public ContactData withHomephone(String homephone) {
+    this.homephone = homephone;
+    return this;
+  }
 
-    public String getFirstname() {
-        return firstname;
-    }
+  public ContactData withAllPhones(String allPhones) {
+    this.allPhones = allPhones;
+    return this;
+  }
 
-    public String getLastname() {
-        return lastname;
-    }
+  public ContactData withEmail(String email) {
+    this.email = email;
+    return this;
+  }
 
-    public String getAddress() {
-        return address;
-    }
-    public String getHomephone() {
-        return homephone;
-    }
+  public ContactData withEmail2(String email2) {
+    this.email2 = email2;
+    return this;
+  }
 
-    public String getWorkphone() {
-        return workphone;
-    }
+  public ContactData withEmail3(String email3) {
+    this.email3 = email3;
+    return this;
+  }
 
-    public String getMobilephone() {
-        return mobilephone;
-    }public String getAllPhones() {
-        return allPhones;
-    }
+  public ContactData withAllEmails(String allEmails) {
+    this.allEmails = allEmails;
+    return this;
+  }
 
-    public String getEmail() {
-        return email;
-    }
-    public String getEmail2() {
-        return email2;
-    }
-    public String getEmail3() {
-        return email3;
-    }
-    public String getAllEmails() {
-        return allEmails;
-    }
-    public File getPhoto() {
-        return photo;
-    }
-    @Override
-    public String toString() {
-        return "ContactData{" +
-                "id='" + id + '\'' +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", address='" + address + '\'' +
-                ", homephone='" + homephone + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+  public int getId() {
+    return id;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+  public String getFirstname() {
+    return firstname;
+  }
 
-        ContactData that = (ContactData) o;
+  public String getLastname() {
+    return lastname;
+  }
 
-        if (id != that.id) return false;
-        if (!Objects.equals(firstname, that.firstname)) return false;
-        return Objects.equals(lastname, that.lastname);
-    }
+  public String getAddress() {
+    return address;
+  }
 
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
-        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-        return result;
-    }
+  public String getHomephone() {
+    return homephone;
+  }
+
+  public String getWorkphone() {
+    return workphone;
+  }
+
+  public String getMobilephone() {
+    return mobilephone;
+  }
+
+  public String getAllPhones() {
+    return allPhones;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public String getEmail3() {
+    return email3;
+  }
+
+  public String getAllEmails() {
+    return allEmails;
+  }
+
+  public File getPhoto() {
+    return photo;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+        "id='" + id + '\'' +
+        ", firstname='" + firstname + '\'' +
+        ", lastname='" + lastname + '\'' +
+        ", address='" + address + '\'' +
+        ", homephone='" + homephone + '\'' +
+        ", email='" + email + '\'' +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (id != that.id) return false;
+    if (!Objects.equals(firstname, that.firstname)) return false;
+    return Objects.equals(lastname, that.lastname);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    return result;
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/ContactCreationTests.java
@@ -1,53 +1,73 @@
 package ru.stqa.learn.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.learn.addressbook.model.ContactData;
 import ru.stqa.learn.addressbook.model.Contacts;
 import ru.stqa.learn.addressbook.model.GroupData;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
-
-    @BeforeMethod
-    public void ensurePrecondition(){
-        app.goTo().groupPage();
-        if (app.group().all().size() ==0) {
-            app.group().create(new GroupData().withName("test1"));
-        }
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader
+          (new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1])
+            .withAddress(split[2]).withHomephone(split[3]).withMobilePhone(split[4]).withWorkphone(split[5])
+            .withEmail(split[6]).withEmail2(split[7]).withEmail3(split[8])});
+      line = reader.readLine();
     }
+    return list.iterator();
+  }
 
-    @Test(enabled = false)
-    public void testContactCreation() throws Exception {
+  @BeforeMethod
+  public void ensurePrecondition() {
+    app.goTo().groupPage();
+    if (app.group().all().size() == 0) {
+      app.group().create(new GroupData().withName("test1"));
+    }
+  }
 
-        app.goTo().homePage();
-        Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/dog.png");
-        ContactData contact = new ContactData().withFirstname("John").withLastname("Doe")
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
+
+    app.goTo().homePage();
+    Contacts before = app.contact().all();
+    File photo = new File("src/test/resources/dog.png");
+       /*ContactData contact = new ContactData().withFirstname("John").withLastname("Doe")
                 .withAddress("333 Spring St").withHomephone("111").withMobilePhone("222")
-                .withWorkphone("333").withEmail("qwerty@gmail.com").withPhoto(photo);
-        app.contact().initContactCreation();
-        app.contact().createContact(contact);
-        Contacts after = app.contact().all();
-        assertThat(after.size(),equalTo( app.contact().count()));
+                .withWorkphone("333").withEmail("qwerty@gmail.com");//.withPhoto(photo);*/
+    app.contact().initContactCreation();
+    app.contact().createContact(contact);
+    Contacts after = app.contact().all();
+    assertThat(after.size(), equalTo(app.contact().count()));
 
-        assertThat(after, equalTo(before
-                .withAdded(contact.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+    assertThat(after, equalTo(before
+          .withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
-    }
-    @Test
-    public void testCurrentDir(){
-        File currentDir = new File(".");
-        System.out.println(currentDir.getAbsolutePath());
-        File photo = new File("src/test/resources/dog.png");
+  }
 
-        System.out.println(photo.getAbsolutePath());
-        System.out.println(photo.exists());
-    }
+  @Test(enabled = false)
+  public void testCurrentDir() {
+    File currentDir = new File(".");
+    System.out.println(currentDir.getAbsolutePath());
+    File photo = new File("src/test/resources/dog.png");
+
+    System.out.println(photo.getAbsolutePath());
+    System.out.println(photo.exists());
+  }
 
 
 }
