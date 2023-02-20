2d022dc67c291b10c362cf80d302a8b427cdc6c2
Pearl1306 <36585813+Pearl1306@users.noreply.github.com>
2023-02-20 06:06:28 -0500

    Implemented the ability to remotely lunch the browser

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,20 @@
 package ru.stqa.learn.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -25,7 +29,7 @@ public class ApplicationManager {
     private String browser;
     private DbHelper dbHelper;
 
-    public ApplicationManager(String browser){
+    public ApplicationManager(String browser) {
         this.browser = browser;
         String target = System.getProperty("target", "local");
         properties = new Properties();
@@ -35,23 +39,32 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-        dbHelper= new DbHelper();
+        dbHelper = new DbHelper();
 
-        if(browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        }else if(browser.equals(BrowserType.CHROME)){
-            wd = new ChromeDriver();
-        }else if(browser.equals(BrowserType.IE)){
-            wd = new InternetExplorerDriver();
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
+
+
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
         contactHelper = new ContactHelper(wd);
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
+
     public void stop() {
         wd.quit();
     }
@@ -77,7 +90,8 @@ public class ApplicationManager {
     public ContactHelper contact() {
         return contactHelper;
     }
-    public DbHelper db(){
+
+    public DbHelper db() {
         return dbHelper;
     }
 }
